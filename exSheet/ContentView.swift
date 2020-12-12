a689517fffe9d263c80f9fabc971e95fdff4851f
Benedito Alberto Macedo <benedito.macedo@gmail.com>
2020-12-12 20:51:08 -0300

    Sheet implemented

--- exSheet/ContentView.swift
+++ exSheet/ContentView.swift
@@ -8,12 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showingDetail = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button(action: {
+            self.showingDetail = true
+        }) {
+            Text("Show detail")
+        }.sheet(isPresented: $showingDetail) {
+            DetailView(showingDetail: self.$showingDetail)
+        }
+        .padding()
+        .border(Color.blue)
     }
 }
 
+struct DetailView: View {
+    @Binding var showingDetail: Bool
+    
+    var body: some View {
+        VStack {
+            Button(action: {
+                self.showingDetail = false
+            }) {
+                Text("Close")
+            }
+            .padding()
+            .border(Color.blue)
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
