21f57fb2f88bfb485c2e979015831945808638a2
Benedito Alberto Macedo <benedito.macedo@gmail.com>
2020-12-11 22:40:06 -0300

    Initial Commit

---
+++ exSheet/exSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  exSheetApp.swift
+//  exSheet
+//
+//  Created by Benedito Macedo on 11/12/20.
+//
+
+import SwiftUI
+
+@main
+struct exSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
